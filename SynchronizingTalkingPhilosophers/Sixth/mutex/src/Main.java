70e3bbd6b1c276fed12e591ee29faf2bdc51cbec
meyssam Rostamzadeh <meyssam.rostamzadeh@gmail.com>
2023-10-27 05:00:25 +0330

    Update Main.java

--- SynchronizingTalkingPhilosophers/Sixth/mutex/src/Main.java
+++ SynchronizingTalkingPhilosophers/Sixth/mutex/src/Main.java
@@ -5,16 +5,12 @@ public class Main {
         int philosophers = Integer.parseInt(args[0]);
         int cycles = Integer.parseInt(args[1]);
         int testNumber = Integer.parseInt(args[2]);
-
-//        var ty = new nclas();
         try {
             Table myTable = new Table(philosophers,cycles,testNumber);
         } catch (TalkingException e) {
             throw new RuntimeException(e);
         }
-//        talkingexception myException = new talkingexception();
-//        phonebooth myPhoneBooth = new phonebooth();
-
+        Table.setDebugLevel(2);
         System.out.println("Number of Philosophers: " + philosophers);
         System.out.println("Number of Cycles: " + cycles);
         System.out.println("Test Number: " + testNumber);
