bc92682c326abeb8e87580162bd65c99db0eedac
meyssam Rostamzadeh <meyssam.rostamzadeh@gmail.com>
2024-01-15 01:38:55 +0330

    add solution txt file

--- MySkipList/SkipList/src/taojava/util/SkipList.java
+++ MySkipList/SkipList/src/taojava/util/SkipList.java
@@ -1,241 +1,247 @@
-
 package taojava.util;
+
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 /**
  * A randomized implementation of sorted lists.
  * @author Samuel A. Rebelsky
  * @author Meyssam Rostamzadeh
+
  */
 public class SkipList<T extends Comparable<T>>
     implements SortedList<T>
 {
-  Node head; //start of skiplist
-  int maxLevel; //max vertical level for each node
-  double probability;
-  int size; //size of skiplist
+  int maxLevel; //Maximum allowed levels in the list
+
+  int levels; // total levels of current node
+
+  Node<T> first;
+
+  Node<T> last;
+  int mods = 0; //Number of modifications made to the SkipList
+  Random random = new Random();
+
+  double probability = 0.5; //used for generating random level for new node
+
 
   public class Node<T>
   {
 
-    T val;
-    Node<T>[] next; // it should be array because each node have multiple level...
-    public Node(T value, int setLevel) //adding new node
+    T val; //actual value of node
+    Node<T>[] forward; //Array of forward pointers, size determined by level of node.
+    @SuppressWarnings("unchecked")
+    public Node(T val, int level) //generate new node
     {
-      this.val = value;
-      next = new Node[setLevel + 1];
-
-      for (int i = 0; i <= setLevel; i++)
+      this.val = val;
+      this.forward = new Node[level + 1];
+      for (int i = 0; i < level; i++)
+        {
+          forward[i] = null;
+        }
+    }
+  }
+  @SuppressWarnings({ "unchecked", "rawtypes" })
+  public SkipList(int max) //Creates a SkipList with a specified maxLevel
+  {
+    //at start, just two node with value == null.
+    // first node point to last node in all levels
+    this.maxLevel = max;
+    this.levels = 0;
+    this.first = new Node(null, maxLevel);
+    this.last = new Node(null, maxLevel);
+    for (int i = 0; i < maxLevel; i++)
       {
-        next[i] = null; // for each level, set next to null
+        this.last.forward[i] = null;
+        this.first.forward[i] = last;
       }
-    }
   }
 
-  public SkipList(int level, double prob)
+  public SkipList() //Creates a SkipList with a maxLevel of 20.
   {
-    this.probability = prob;
-    this.maxLevel = level;
-    this.head = new Node(null, maxLevel);
-    this.size = 0;
+    this(20);
   }
-
-  public int randomLevel()
+  private int randomLevel() //generate a random level for new node
   {
-    int newLevel = 1;
-    Random value = new Random();
-    while (value.nextDouble() < this.probability)
-    {
-      newLevel++;
-    }// while random is less than the probability
-    return Math.min(newLevel, this.maxLevel);
+    int newLevel = 0;
+    while (this.random.nextDouble() < this.probability)
+      {
+        newLevel = newLevel + 1;
+      }
+    return Math.min(newLevel, this.maxLevel - 1);
   }
-
-
-  public class SkipListIterator implements Iterator
+  @SuppressWarnings("unused")
+  private boolean hasNext(Node<T> node, int level)
   {
-    Node current;
-    public SkipListIterator()
-    {
-      current = head;
-    }
-
-    public boolean hasNext() // Check if the iterator has a next element.
-    {
-      return current.next[0] != null;
-    }
-
-    @SuppressWarnings("unchecked")
-    public T next() //Return the next element.
-    {
-      current = current.next[0];
-      return (T) current.val;
-    }
-
-
-    @SuppressWarnings("unchecked")
-    public void remove() //Remove the current element.
-    {
-      SkipList.this.remove((T) this.current.val);
-    }
+    return node.forward[level].val != null;
   }
 
-  @SuppressWarnings("unchecked")
+  /**
+   * Return a read-only iterator (one that does not implement the remove
+   * method) that iterates the values of the list from smallest to
+   * largest.
+   */
   public Iterator<T> iterator()
   {
-    return new SkipListIterator();
+    return new Iterator<T>()
+      {
+        /**
+         * The node that immediately precedes the value to be returned by
+         * next.
+         */
+        Node<T> cursor = SkipList.this.first;
+
+        int mods = SkipList.this.mods; //The number of modifications at the time this iterator was created or last updated.
+
+        void failFast() //Determine if the list has been updated since this iterator was created or modified.
+        {
+          if (this.mods != SkipList.this.mods)
+            throw new ConcurrentModificationException();
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+          failFast();
+          return SkipList.this.hasNext(cursor, 0);
+        }
+
+        @Override
+        public T next()
+        {
+          failFast();
+          if (!this.hasNext())
+            throw new NoSuchElementException();
+          // Advance to the next node.
+          this.cursor = this.cursor.forward[0];
+          // Return the data in the now current node.
+          return this.cursor.val;
+        }
+
+        @Override
+        public void remove()
+        {
+          T val = cursor.val;
+          SkipList.this.remove(val);
+          this.mods++;
+        }
+      };
   }
 
   public void add(T val)
   {
-    if (val == null)
-    {
+    int newLevel = randomLevel();
+    @SuppressWarnings("unchecked")
+    Node<T>[] update = new Node[this.maxLevel];
+    Node<T> newNode = new Node<T>(val, newLevel);
+    Node<T> tmp = this.first;
+    for (int i = this.levels; i >= 0; i--)
+      {
+        while (hasNext(tmp, i) && tmp.forward[i].val.compareTo(val) < 0)
+          {
+            tmp = tmp.forward[i];
+          }
+        update[i] = tmp;
+      }
+    if (hasNext(tmp, 0) && tmp.forward[0].val.compareTo(val) == 0)
       return;
-    } // if value is null, don't add anything
-
-    Node current = this.head;
-    Node[] update = new Node[this.maxLevel + 1];
-
-    for (int level = this.maxLevel; level >= 0; level--)
-    {
-      while ((current.next[level] != null)
-              && val.compareTo((T) current.next[level].val) > 0)
+    // if the newLevel is bigger than levels
+    if (newLevel > this.levels)
       {
-        current = current.next[level];
-      } //search for the preceding node
-      update[level] = current;
-    } // for all levels
-
-    current = current.next[0];
-
-    if (current != null && val.compareTo((T) current.val) == 0)
-    {
-      return;
-    } // if already exists don't add
-    else
-    {
-      //create a new node with a random level
-      int newLevel = randomLevel();
-
-      if (newLevel > this.maxLevel)
+        for (int i = this.levels + 1; i <= newLevel; i++)
+          {
+            update[i] = this.first;
+          } // for
+        this.levels = newLevel;
+      } // if 
+    for (int i = 0; i <= newLevel; i++)
       {
-        for (int level = this.maxLevel + 1; level <= newLevel; level--)
-        {
-          update[level] = this.head;
-        } // for
-        this.maxLevel = newLevel;
-      } // if, new level is greater than maxlevel
-
-      current = new Node(val, newLevel);
-
-      for (int level = 0; level <= newLevel; level++)
-      {
-        current.next[level] = update[level].next[level];
-        update[level].next[level] = current;
-      } // update the pointers
-    } // else
-    this.size++;
-  } // add(T val)
-
-
-  @SuppressWarnings("unchecked")
+        newNode.forward[i] = update[i].forward[i];
+        update[i].forward[i] = newNode;
+      }
+    this.mods++;
+  }
+  @SuppressWarnings("unused")
   public boolean contains(T val)
   {
-    Node<T> current = this.head;
-
-    if (val == null)
-    {
-      return false;
-    } // if searching for null return false
-    else
-    {
-      for (int level = this.maxLevel; level >= 0; level--)
+    Node<T> tmp = this.first;
+    for (int i = this.levels; i >= 0; i--)
       {
-        while ((current.next[level] != null)
-                && val.compareTo((T) current.next[level].val) > 0)
-        {
-          current = current.next[level];
-        } // while
-      } // for all  levels
-      current = current.next[0];
-      // return val.equals(current.val);
-    } // else if val is not null
-
-    if (current == null) // if, not found
-      return false;
+        while (hasNext(tmp, i) && tmp.forward[i].val.compareTo(val) < 0)
+          {
+            tmp = tmp.forward[i];
+          }
+      }
+    if(!hasNext(tmp, 0))
+      {
+        return false;
+      }
+    tmp = tmp.forward[0];
+    if (tmp.val.compareTo(val) == 0)
+      return true;
     else
-      return val.equals(current.val);
-  } // contains(T)
+      return false;
+  }
 
 
+  @SuppressWarnings({ "unused", "unchecked" })
   public void remove(T val)
   {
-    if (val == null)
-    {
-      return;
-    } // if value is null don't remove anything
-
-    Node current = this.head;
-    Node[] update = new Node[this.maxLevel + 1];
-
-    for (int level = this.maxLevel; level >= 0; level--)
-    {
-      while ((current.next[level] != null) && val.compareTo((T) current.next[level].val) > 0)
+    Node<T>[] update = new Node[maxLevel];
+    Node<T> tmp = this.first;
+    for (int i = this.levels; i >= 0; i--)
       {
-        current = current.next[level];
-      } // while, finding the preceding nodes
-      update[level] = current;
-    } // for all levels
-
-    current = current.next[0];
-
-    if (current != null && val.compareTo((T) current.val) == 0)
-    {
-      for (int i = 0; i <= this.maxLevel; i++)
+        while (hasNext(tmp, i) && tmp.forward[i].val.compareTo(val) < 0)
+          {
+            tmp = tmp.forward[i];
+          }
+        update[i] = tmp;
+      }
+    tmp = tmp.forward[0];
+    if (tmp.val != null && tmp.val.compareTo(val) == 0)
       {
-        if (update[i].next[i] != current)
-          break;
-        update[i].next[i] = current.next[i];
-      } //for, all the levels update the pointers
-      current = null;
-      while ((this.maxLevel > 1) && (this.head.next[this.maxLevel] == null))
-      {
-        this.maxLevel--;
-      } // while
-
-      this.size--;
-      return;
-    } // if, the val exists, then remove it
-    else
-    {
-      return;
-    } // else, if not found
-  }// remove(T)
-
-
-  public T get(int i) //Get the element at index i.
+        for (int i = 0; i <= this.levels; i++)
+          {
+            if (!update[i].forward[i].equals(tmp))
+              break;
+            update[i].forward[i] = tmp.forward[i];
+          }
+        while (this.levels > 0 && this.first.forward[this.levels].val == null)
+          {
+            this.levels--;
+          }
+        this.mods++;
+      }
+  }
+  public T get(int i)
   {
-    if ((i < 0) || (i > this.size))
-    {
-      return null; //index out of bounds
-    }
-
-    Node current = this.head.next[0];
-    if (current == null)
-    {
-      return null;
-    } // if list is empty
-
-    for (int pos = 0; pos < i; pos++)
-    {
-      current = current.next[0];
-    } // loop through to the index
-    return (T) current.val;
+    Node<T> tmp = this.first;
+    for (int j = 0; j < i; j++)
+      {
+        if (!hasNext(tmp, 0))
+          {
+            throw new IndexOutOfBoundsException();
+          }
+        tmp = tmp.forward[0];
+      }
+    if (!hasNext(tmp, 0))
+      {
+        throw new IndexOutOfBoundsException();
+      } // if
+    return tmp.forward[0].val;
+  }
+  public int length()
+  {
+    int length = 0;
+    Node<T> tmp = this.first;
+    while (hasNext(tmp, 0))
+      {
+        tmp = tmp.forward[0];
+        length++;
+      }
+    return length;
   }
 
-  public int length() //Determine the number of elements in the collection.
-  {
-    return this.size;
-  }
 }
